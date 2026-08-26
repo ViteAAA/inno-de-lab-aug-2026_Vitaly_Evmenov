a7ef5d8a58e3a054bf73321e996f542a083740a6
ViteAAA <evmenovvitalj@gmail.com>
2026-08-26 23:07:50 +0300

    fixed task_3

--- inno-de-lab_task_6/Task_3/main.py
+++ inno-de-lab_task_6/Task_3/main.py
@@ -5,4 +5,4 @@ degree_in_cel = float(input("Введите температуру в граду
 degree_in_fahrenheit = degree_in_cel * 9 / 5 + 32
 
 # output result
-print(f"{degree_in_cel}°C это {degree_in_fahrenheit} градусов по Форрингейту")
+print(f"{degree_in_cel}°C это {degree_in_fahrenheit} градусов по Фаренгейту")
